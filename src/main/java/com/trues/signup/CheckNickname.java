c2dedd412f9645a9c2d3695a15a0d34384b68872
Ra Hyeon Ji <119282494+raxchaz@users.noreply.github.com>
2023-06-11 21:44:06 +0900

    닉네임 중복 확인 기능 구현 닉네임 공백 확인 기능 구현 닉네임 글자 수 제한 기능 구현
    + 닉네임 기능 테스트 코드 작성

---
+++ src/main/java/com/trues/signup/CheckNickname.java
@@ -0,0 +1,27 @@
+package com.trues.signup;
+
+public class CheckNickname {
+
+    // 중복을 확인하는 코드 #HashSet (중복을 자체적으로 확인해줌)
+    // boolean을 사용하여 true, false값을 반환하도록 함
+    public boolean checkNick(String nick) {
+        if (Users.nickSet.contains(nick)) {
+            return false;
+        }
+
+        // 닉네임에 공백이 포함되어 있는가??
+        // 닉네임에 공백이 포함되어 있다면 false를 반환, 공백이 없으면 true; 반환
+        boolean hasSpace = nick.contains("");
+        if (hasSpace) {
+            return false;
+        }
+
+
+        // 최소 3글자 이상으로 작성해야한다.
+        if (nick.length() >= 3) {
+            return true;
+        } else {
+            return false;
+        }
+    }
+}
